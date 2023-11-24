bf5ead9679238395b70c3b2b842a3887b4b501ab
Marco Callado <47508053+callado5@users.noreply.github.com>
2023-11-24 13:07:13 -0300

    Update EstacaoMet.m
    Globbaly

--- EstacaoMet.m
+++ EstacaoMet.m
@@ -1,19 +1,22 @@
-clear all; close all;
-%Dados estação
+%Loading the station data
 
-Dados = readtable('C:\Users\mavca\Downloads\rel_24-11-23_08-27-08.xls'); %Abrindo matrix de dados
-Dia   = table2array(Dados(1:end-3,1)); %Separando dia e Hora
+Dados = readtable('rel_24-11-23_08-27-08.xls'); % put your excel, csv or txt file here
+Dia   = table2array(Dados(1:end-3,1)); % Separating day and hour and deleting my 3 last rows (mean, sum, median)
 Hora  = table2array(Dados(1:end-3,2));
 
-Time  = string(Dia(:))+' '+string(Hora(:)); %Juntando eles em formato de string para ler com datetime
-Time = datetime(Time);
-Data = table2array(Dados(1:end-3,3:end));
-Data(:,end+1) = datenum(Time);
+Time  = string(Dia(:))+' '+string(Hora(:)); %Grouping them in a string
+Time = datetime(Time); % Transforming to datetime
 
-[~,Tsort] = sort(Time); %Pegando ordenamento baseado nas datas (index)
-Data = Data(Tsort,:); % Ordenando os dados baseado na matrix de tempo
-Time = sort(Time); % Ordenando o tempo
-%% Suavizando plot (interpolação)
+Data = table2array(Dados(1:end-3,3:end)); % Loading my meteoroligcal data to array
+
+%% In case my data is not cronological as my time, the script begins to sort your data cronologicaly
+
+[~,Tsort] = sort(Time); %Getting my sorted index based on my datetime vector
+Data = Data(Tsort,:); % Sorting Data by the datetime vector
+Time = sort(Time); % Sorting my datetime
+
+%% Smoothing my data for a smooth line plot (interpolation)
+
 t = datenum(Time);
 samplingRateIncrease = 20;
 newt = linspace(min(t), max(t), length(t) * samplingRateIncrease)';
@@ -22,13 +25,13 @@ for i = 1:length(Data(1,:))
     sData(:,i) = spline(t, Data(:,i), newt);
 end
 
-% 
- plot(newt,sData(:,2),'color','#053B50','LineStyle','--') %Temp
- hold on
- plot(datenum(Time),Data(:,2),'color','#176B87') %Temp
- datetick('x','dd-mmm HH:MM','keepticks')
+%Testing my smoothed plot
+plot(newt,sData(:,2),'color','#053B50','LineStyle','--') %Temp
+hold on
+plot(datenum(Time),Data(:,2),'color','#176B87') %Temp
+datetick('x','dd-mmm HH:MM','keepticks')
 
-%% Testando Plots
+%% Testing the plots
 
 figure(1)
 plot(newt,sData(:,2),'color','#176B87') %Temp
@@ -42,7 +45,9 @@ figure(2)
 bar(Time,Data(:,1),'EdgeColor','none','FaceColor','#64CCC5');%Precip
 title('Precipitação (mm)')
 
-%%
+%% For my wind rose plot please check: https://dpereira.asempyme.com/windrose/
+% And for my colormaps i used: https://www.mathworks.com/matlabcentral/fileexchange/120088-200-colormap
+
 addpath('C:\Users\mavca\OneDrive\Documentos\Cotonho\Rotinas MAT\COLORMAPS\slanCM\slanCM')
 Options = {'anglenorth', 0, 'angleeast', 90, 'labels',{'N','NE','E','SE','S','SW','W','NW'}, 'freqlabelangle', 50,...
     'TitleString','Velocidade dos ventos Água Preta','LabLegend', 'Intensidade m/s','nDirections',15,...
